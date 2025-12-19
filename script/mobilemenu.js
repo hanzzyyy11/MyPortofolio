242ee4b6f0db926aa1c10b0465503275911823e9
hanzzyyy11 <hanzzyyy137@gmail.com>
2025-12-19 14:27:14 +0800

    Update mobilemenu.js

--- script/mobilemenu.js
+++ script/mobilemenu.js
@@ -1,14 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const menu = document.querySelector(".mobile-menu");
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".mobile-menu");
 
-  if (!hamburger || !menu) {
-    console.log("Hamburger / menu tidak ketemu");
-    return;
-  }
-
+if (hamburger && menu) {
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
   });
-});
+}
