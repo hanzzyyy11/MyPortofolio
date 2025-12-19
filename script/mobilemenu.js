3bf557c995ca15e263198c7f38a5fe3a42a2f8a0
hanzzyyy11 <hanzzyyy137@gmail.com>
2025-12-19 14:12:11 +0800

    Update mobilemenu.js

--- script/mobilemenu.js
+++ script/mobilemenu.js
@@ -1,1 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  /* ===== MOBILE MENU ===== */
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.querySelector(".mobile-menu");
+
+  if (hamburger && menu) {
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      menu.classList.toggle("active");
+    });
+  }
